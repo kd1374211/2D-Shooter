e55bcdc698df7dd369cce7ed2a1515489f9a2f74
gaku <kd1374211@st.kobedenshi.ac.jp>
2026-04-29 13:18:18 +0900

    タイトル
    タイトル画面の作成とUIの素材準備

--- スケルトンプログラム/Src/Application/Scene/TitleScene.cpp
+++ スケルトンプログラム/Src/Application/Scene/TitleScene.cpp
@@ -1,14 +1,38 @@
 #include "TitleScene.h"
 #include "../Key/KeyStateManager.h"
 #include "SceneManager.h"
+#include "../Background/Background.h"
 
-C_TitleScene::C_TitleScene()
+C_TitleScene::C_TitleScene() :m_alpha(1.0f), m_alphaChangeMulti(-1)
 {
 	SetSceneTag(E_SceneTypeTag::Title);
+
+	m_back = new C_Background();
+	m_titleTex.Load("Texture/Scene/TitleScreen.png");
+	m_startButtonTex.Load("Texture/Scene/PressEnter.png");
+}
+
+C_TitleScene::~C_TitleScene()
+{
+	if (m_back)
+	{
+		delete m_back;
+		m_back = nullptr;
+	}
+	m_titleTex.Release();
+	m_startButtonTex.Release();
 }
 
 void C_TitleScene::Update()
 {
+	m_back->Update();
+
+	m_alpha += ALPHACHANGE * m_alphaChangeMulti;
+	if (m_alpha < ALPHAMIN || m_alpha > ALPHAMAX)
+	{
+		m_alphaChangeMulti *= -1;
+	}
+
 	if (KEYMGR.GetKeyState(E_KeyChecks::Enter) == E_KeyState::Released)
 	{
 		SCENEMGR.SetSceneQueue(E_SceneTypeTag::Select);
@@ -17,4 +41,19 @@ void C_TitleScene::Update()
 
 void C_TitleScene::Draw()
 {
+	//背景
+	m_back->Draw();
+
+	//タイトル
+	long width = m_titleTex.GetInfo().Width;
+	long height = m_titleTex.GetInfo().Height;
+	Math::Rectangle rec = { 0,0,width,height };
+	SHADER.m_spriteShader.DrawTex(&m_titleTex, 0, 0, width, height, &rec);
+
+	//スタート
+	width = m_startButtonTex.GetInfo().Width;
+	height = m_startButtonTex.GetInfo().Height;
+	rec = { 0,0,width,height };
+	Math::Color color = { 1,1,1,m_alpha };
+	SHADER.m_spriteShader.DrawTex(&m_startButtonTex, 0, 0, width, height, &rec, &color);
 }
--- スケルトンプログラム/Src/Application/Scene/TitleScene.h
+++ スケルトンプログラム/Src/Application/Scene/TitleScene.h
@@ -1,15 +1,28 @@
 #pragma once
 #include "SceneBase.h"
 
+class C_Background;
+
 class C_TitleScene :public C_SceneBase
 {
 public:
 
 	C_TitleScene();
-	~C_TitleScene()override {};
+	~C_TitleScene()override;
 
 	void Update()override;
 	void Draw()override;
 
 private:
+
+	const float ALPHACHANGE = 0.02f;
+	const float ALPHAMIN = 0.2f;
+	const float ALPHAMAX = 1.0f;
+
+	C_Background* m_back = nullptr;
+	KdTexture m_titleTex;
+	KdTexture m_startButtonTex;
+	float m_alpha;
+	int m_alphaChangeMulti;
+
 };
